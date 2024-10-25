9572d6cfb3ccf6ea18dd64e520eb4a1750b2efc2
Maria Amigo <maria.amigo.07@gmail.com>
2024-10-25 17:11:52 +0200

    Ajout de commentaires + ajout d'une condition dans le middleware linkImage

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -3,22 +3,39 @@ const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
 
+// Configuration du stockage en mémoire avec Multer
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+// Middleware pour traiter l'image après le téléchargement
 const linkImage = async(req, res, next) => {
-    const imagesDir = path.join(__dirname, '../images'); 
+    if (!req.file) {
+        // Passage au middleware suivant si aucun fichier
+        return next();
+    }
+
+    // Répertoire où les images seront enregistrées
+    const imagesDir = path.join(__dirname, '../images');
+    
+    // Vérification si le répertoire existe déjà
     fs.access(imagesDir, (error) => {
         if (error) {
+          // Création du répertoire
           fs.mkdirSync(imagesDir);
         }
     });
+
+    // Extraction des informations du fichier téléchargé
     const { buffer, originalname } = req.file;
     const timestamp = Date.now();
     const fileName = `${originalname.split(' ').join('_')}-${timestamp}.webp`;
+
+    // Traitement de l'image pour la convertir au format WebP avec une qualité de 20%
     await sharp(buffer)
         .webp({ quality: 20 })
         .toFile("./images/" + fileName);
+
+    // Construction de l'URL accessible de l'image traitée
     req.processedImageUrl = `${req.protocol}://${req.get('host')}/images/${fileName}`
     next();
 }
